1b07ddce23be8cf0b05eb3d43308277533f04cfa
Andrea Blanco <estefania_8_3@hotmail.com>
2021-11-06 13:51:07 -0500

    style: add search view style

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -10,12 +10,12 @@ const Search = () => {
   }
 
   return (
-    <Container className='contentSearch' fluid>
+    <Container fluid>
       <Row>
-        <Col xs={18}>
-          <InputGroup size="lg">
-            <InputGroup.Text id="inputGroup-sizing-lg">Buscador</InputGroup.Text>
-            <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" onChange={onSearchValueChange} value={searchValue}/>
+        <Col xs={12} sm={12} md={10} lg={10} xl={8} xxl={8}>
+          <InputGroup size="lg" className="inputGroup">
+            <InputGroup.Text id="inputGroup-sizing-lg" className="labelSearch"><i className="fas fa-search iconSearch"></i></InputGroup.Text>
+            <FormControl className="inputSearch" placeholder="Escribe aquí..." aria-label="Large" aria-describedby="inputGroup-sizing-sm" onChange={onSearchValueChange} value={searchValue} />
           </InputGroup>
         </Col>
       </Row>
