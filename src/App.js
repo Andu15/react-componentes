2a55b3784efcc1188c768644e07a747702a698d2
Andrea Blanco <estefania_8_3@hotmail.com>
2021-11-06 18:28:29 -0500

    style: add react-bootstrap style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import  Search from './components/Search/index';
 
 function App() {
